c88ed7ec915de355111e74343a6ce1eaace1ebfa
Rishvanth <rishvanthrr@gmail.com>
2025-12-23 12:30:09 +0530

    Added new  header

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ def main():
     print("Welcome to DevOps SCM Project")
     print("Feature branch: New Header added")
     print("DevOps Lab Exercise 4")
+    print("New Header Added")
 
 if __name__ == "__main__":
     main()
